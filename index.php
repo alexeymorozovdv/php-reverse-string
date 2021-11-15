124c0a2fe27a8fbb50925f72b4d99ea8a03bc3dc
Alexey Morozov <alexeymorozov.dev@gmail.com>
2021-11-15 18:29:48 +0300

    Added to the index.php file html form

--- index.php
+++ index.php
@@ -2,6 +2,36 @@
 
 require_once 'ReverseString.php';
 
-$result = new ReverseString();
-echo $result->reverse("Привет! Давно не виделись."); // Тевирп! Онвад ен ьсиледив.
+if (isset($_POST['string']) && !empty($_POST['string'])) {
+    $string = $_POST['string'];
+    $result = ReverseString::reverse($string);
+}
 
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String Reverse</title>
+</head>
+<body>
+
+<form action="/" method="post">
+    <label for="string">Введите строку</label>
+    <input type="text" name="string">
+    <input type="submit" value="Revert!">
+</form>
+
+<?php
+    if ($result) { ?>
+        <p>Дана строка: <?= $string ?></p>
+        <p>Результат: <?= $result ?></p>
+    <?php }
+?>
+
+</body>
+</html>
